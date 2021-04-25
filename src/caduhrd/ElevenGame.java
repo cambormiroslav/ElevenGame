c6cf16f4b6cf97b22790e2502d6969e8e52252cc
cambormiroslav <miroslav.cambor@tul.cz>
2021-04-25 16:13:20 +0200

    propojeni s interfacem GameInterface z baliku elevengame

--- src/caduhrd/ElevenGame.java
+++ src/caduhrd/ElevenGame.java
@@ -1,9 +1,46 @@
 package caduhrd;
 
+import elevengame.*;
+import java.util.List;
 /**
  *
  * @author Cambor, Duracka, Hrdlicka
  */
-public class ElevenGame {
+public class ElevenGame implements GameInterface{
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int nCards() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getDeckSize() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getCardDescriptionAt(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean anotherPlayIsPossible() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean playAndReplace(List<Integer> iSelectedCards) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isWon() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
