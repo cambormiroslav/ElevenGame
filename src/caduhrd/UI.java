38904b7ee07035e1d594b78b95c49cc1cca50569
cambormiroslav <miroslav.cambor@tul.cz>
2021-04-25 16:04:52 +0200

    premena tridy na main class

--- src/caduhrd/UI.java
+++ src/caduhrd/UI.java
@@ -1,15 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package caduhrd;
 
 /**
  *
- * @author Milan Ďuračka
+ * @author Cambor, Duracka, Hrdlicka
  */
 public class UI {
+
     //System.out.println("hello world");
     //coment
+    public static void main(String[] args) {
+
+    }
 }
